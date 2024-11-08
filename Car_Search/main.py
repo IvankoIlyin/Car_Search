da98aaac924dd275d491ef1d60ddf56451ee62fc
Ivanko <52660349+IvankoIlyin@users.noreply.github.com>
2024-11-08 21:34:15 +0200

    Update main.py

--- Car_Search/main.py
+++ Car_Search/main.py
@@ -6,27 +6,11 @@ from aiogram import Bot, Dispatcher
 from handlers import questions
 
 
-# car_char=car_obj.Car_Characteristics()
-# car_char.add_attr("Тип палива","Бензин")
-# car_char.add_attr("Коробка","Механіка")
-# car_char.add_attr("Тип кузова","Хетчбек")
-# car_char.add_attr("Тип кузова","Седан")
-# car_char.add_attr("Тип кузова","Кросовер")
-# car_char.add_attr("Привід","Передній")
-# car_char.add_attr("Колір","Чорний")
-# car_char.add_attr("Пробіг","300")
-# car_char.add_attr("Двигун","2")
-# car=car_obj.Car("skoda","octavia",["3000","10000"],["2000","2015"],car_char,"Продам авто Skoda Oktavia a 5 машина в хорошому стані мотор працює добре коробка передач супер масла фільтра замінені")
-#
-# # Color: Red,Black
-# list = scrapy_parse_car.start_parse_car_site(car)
-#
-# for i in list:
-#     print(i.link)
+
 
 
 async def main():
-    bot = Bot(token="6522837095:AAGpGmy2TBwrXadFC-lApyqFkgUcS3SFOV0")
+    bot = Bot(token="")
     dp = Dispatcher()
 
     dp.include_router(questions.router)
