4abec0818bbcb0b7fb2421bf2f0f469f21acf4e0
Ivanko <52660349+IvankoIlyin@users.noreply.github.com>
2026-03-09 21:59:58 +0200

    ...

--- Car_Search/main.py
+++ Car_Search/main.py
@@ -10,7 +10,7 @@ from handlers import questions
 
 
 async def main():
-    bot = Bot(token="6522837095:AAGpGmy2TBwrXadFC-lApyqFkgUcS3SFOV0")
+    bot = Bot(token="TOKEN")
     dp = Dispatcher()
 
     dp.include_router(questions.router)
